6d27a984d0e33b49c085441623b0a489518ff73f
Spencer Judd <spencercjudd@gmail.com>
2026-05-29 16:59:42 -0400

    Fix typo in index.js

--- go-web-app/assets/js/index.js
+++ go-web-app/assets/js/index.js
@@ -5,7 +5,7 @@ $("#app").html(
     "<p>This app was compiled using Task.</p>" +
     "<p>" +
     `It uses <a target="_blank" href="https://esbuild.github.io/">esbuild</a> to bundle JS and CSS assets, ` +
-    `and <a target="_blank" href="https://pkg.go.dev/embed">Go embeb</a> to keep ` +
+    `and <a target="_blank" href="https://pkg.go.dev/embed">Go embed</a> to keep ` +
     "everything (assets and templates) inside a single binary." +
     "</p>"
 );
